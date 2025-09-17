eb3b595dfb75afa3fe047bf70f0d8e9ef0b2ef31
Tom Peer <tom@clik.com>
2025-09-17 18:46:00 +0100

    Lots of stuff was broken - especially grids

--- _assets/js/clik_common.js
+++ _assets/js/clik_common.js
@@ -178,10 +178,45 @@ clik = {
 				if (Number.isNaN(val)) {
 					return;
 				}
+				break;
 			case "string":
 				val = val.replace(/^"|"$/g, '');
 		}
 		
 		return val;
+	},
+	/*
+	Return settings for a CS item from the CSS
+
+	## Synopsis
+
+	For every property defined in styledefs (these are saved in clik_settings.js)
+	we check if it's a "setting" as opposed to a plain style and then fetch
+	it from the CSS if it is.
+
+	*/
+	getSettings: function ($elem, type) {
+		if (! clik_settings || ! type in clik_settings) {
+			console.warn("Settings not defined");
+			return;
+		}
+		
+		let settings = {};
+		
+		for (let setting in clik_settings[type].styleDefs) {
+			if ( clik_settings[type].styleDefs[setting].setting ) {
+				let val = $elem.css("--" + setting);
+				if (! val) {
+					if ("default" in clik_settings[type].styleDefs[setting]) {
+						val = clik_settings[type].styleDefs[setting].default;
+					}
+				}
+				if ( val ) {
+					settings[setting] = clik.parseCssVar(val, clik_settings[type].styleDefs[setting].type.toLowerCase() );
+				}
+			}
+		}
+			
+		return settings;
 	}
 };
